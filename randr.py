d83f9c955d05b890ae62456f5c5e21b4cd09465f
Cihangir Akturk <cihangir.akturk@tubitak.gov.tr>
2016-01-14 18:18:04 +0200

    At this point we can handle most of the features of xrandr
    Signed-off-by: Cihangir Akturk <cihangir.akturk@tubitak.gov.tr>

--- randr.py
+++ randr.py
@@ -14,6 +14,9 @@ class Mode(object):
         self.current = current
         self.preferred = preferred
 
+    def get_resolution(self):
+        return (self.width, self.height)
+
     def __str__(self):
         return '{0}x{1}, {2}, curr: {3}, pref: {4}'.format(self.width, \
                 self.height, self.freq, self.current, self.preferred)
@@ -22,6 +25,20 @@ class Mode(object):
         return '{0}x{1}'.format(self.width, self.height)
 
     __repr__ = __str__
+
+class ScreenSettings(object):
+    """docstring for ScreenSettings"""
+    def __init__(self):
+        super(ScreenSettings, self).__init__()
+        self.reset()
+
+    def reset(self):
+        self.resolution = None
+        self.is_primary = False
+        self.is_enabled = True
+        self.rotation = None
+        self.position = None
+        self.dirty = False
 
 class Screen(object):
     def __init__(self, name, primary, rot, modes):
@@ -35,17 +52,128 @@ class Screen(object):
         for r in modes:
             if r.current:
                 self.rotation = rot
+                self.curr_mode = r
+                break
 
         # list of Modes (width, height)
         self.supported_modes = modes
 
+        self.set = ScreenSettings()
+        self.set.is_enabled = self.is_enabled()
+
     def is_connected(self):
         return len(self.supported_modes) != 0
 
+    def is_enabled(self):
+        for m in self.supported_modes:
+            if m.current:
+                return True
+        return False
+
+    def get_available_resolutions(self):
+        return [(r.width, r.height) for r in self.supported_modes]
+
+    def check_resolution(self, newres):
+        if newres not in self.get_available_resolutions():
+            raise ValueError('Requested resolution is not supported', newres)
+
+    def set_resolution(self, newres):
+        """Sets the resolution of this screen to the supplied
+           @newres parameter.
+
+        :newres: must be a tuple in the form (width, height)
+
+        """
+        if not self.is_enabled():
+            raise ValueError('The Screen is off')
+
+        self.check_resolution(newres)
+        self.set.resolution = newres
+
+    def set_as_primary(self,  is_primary):
+        """Set this monitor as primary
+
+        :is_primary: bool
+
+        """
+        self.set.is_primary = is_primary
+
+    def set_enabled(self, enable):
+        """Enable or disable the output
+
+        :enable: bool
+
+        """
+        self.set.is_enabled = enable
+
+    def rotate(self, direction):
+        """Rotate the output in the specified direction
+
+        :direction: one of (normal, left, right, inverted)
+
+        """
+        self.set.rotation = direction
+
+    def set_position(self, relation, relative_to):
+        """Position the output relative to the position
+        of another output.
+
+        :relation: TODO
+        :relative_to: output name (LVDS1, HDMI eg.)
+        """
+        self.set.position = (relation, relative_to)
+
+    def apply_settings(self):
+        if not self.name:
+            raise ValueError('Cannot apply settings without screen name', \
+                             self.name)
+        if self.set.resolution:
+            self.check_resolution(self.set.resolution)
+
+        has_changed = False
+
+        cmd = ['xrandr', '--output', self.name]
+
+        # set resolution
+        if self.curr_mode.get_resolution() == self.set.resolution \
+                or not self.set.resolution:
+            cmd.append('--auto')
+        else:
+            res = self.set.resolution
+            cmd.extend(['--mode', '{0}x{1}'.format(res[0], res[1])])
+            has_changed = True
+
+        # Check if this screen is already primary
+        if not self.primary and self.set.is_primary:
+            cmd.append('--primary')
+            has_changed = True
+
+        if self.set.rotation and self.set.rotation is not self.rotation:
+            rot = rot_to_str(self.set.rotation)
+            if not rot:
+                raise ValueError('Invalid rotation value', \
+                                 rot, self.set.rotation)
+            cmd.extend(['--rotate', rot])
+            has_changed = True
+
+        if self.set.position:
+            rel, rel_to = self.set.position
+            rel = pos_to_str(rel)
+            cmd.extend([rel, rel_to])
+            has_changed = True
+
+        if self.is_enabled() and not self.set.is_enabled:
+            if has_changed:
+                raise Exception('--off: this option cannot be combined ' \
+                                'with other options')
+            cmd.append('--off')
+            has_changed = True
+
     def __str__(self):
-        return '{0}, primary: {1}, modes: {2}, conn: {3}, rot: {4}'.format( \
-                self.name, self.primary, len(self.supported_modes), \
-                self.is_connected(), rot_to_str(self.rotation))
+        return '{0}, primary: {1}, modes: {2}, conn: {3}, rot: {4}, '\
+                'enabled: {5}'.format(self.name, self.primary, \
+                len(self.supported_modes), self.is_connected(), \
+                rot_to_str(self.rotation), self.is_enabled())
 
     __repr__ = __str__
 
@@ -53,8 +181,7 @@ class RotateDirection(object):
     Normal, Left, Inverted, Right = range(1, 5)
     valtoname = {Normal:'normal', Left:'left', Inverted:'inverted', \
             Right:'right'}
-    nametoval = {'normal':Normal, 'left':Left, 'inverted':Inverted, \
-            'right':Right}
+    nametoval = dict((v, k) for k, v in valtoname.iteritems())
 
 def rot_to_str(rot):
     if rot in RotateDirection.valtoname:
@@ -66,6 +193,17 @@ def str_to_rot(s):
         return RotateDirection.nametoval[s]
     return RotateDirection.Normal
 
+class PostitonType(object):
+    LeftOf, RightOf, Above, Below, SameAs = range(1, 6)
+    valtoname = {LeftOf:'--left-of', RightOf:'--right-of', Above:'--above', \
+                 Below:'--below', SameAs:'--same-as'}
+    nametoval = dict((v, k) for k, v in valtoname.iteritems())
+
+def pos_to_str(n):
+    return PostitonType.valtoname[n]
+
+def str_to_pos(s):
+    return PostitonType.nametoval[s]
 
 def exec_cmd(cmd):
     # throws exception CalledProcessError
@@ -125,6 +263,14 @@ def parse_xrandr(lines):
         screens.append(create_screen(sc_name_line, modes))
 
     return screens
+
+def get_connected_screens():
+    """Get connected screens
+    """
+    return [s for s in parse_xrandr(exec_cmd('xrandr')) if s.is_connected()]
+
+def get_enabled_screens():
+    return [s for s in get_connected_screens() if s.is_enabled()]
 
 def main():
     print("main entry point\n================")
